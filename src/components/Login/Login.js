81589c6d0f26532dce7203e74ceb23d912aa2a44
jannatunnaeem141 <jannatunnaeem141@gmail.com>
2022-04-14 21:07:31 +0600

    login form designed

---
+++ src/components/Login/Login.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Login.css';
+
+const Login = () => {
+    return (
+        <div className='form-container'>
+            <div>
+                <h2 className='form-title'>Login</h2>
+                <form action="">
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name='password' />
+                    </div>
+                    <input className='form-submit' type="submit" value="Login" />
+                </form>
+                <p>New to Ema-John? <Link className='form-link' to='/signup'>Create an account</Link></p>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
